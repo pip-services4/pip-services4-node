cc6a9d7daa9aff16030d7444484018b0a12cc3c7
Sergey Seroukhov <sergey.seroukhov@spirent.com>
2023-05-26 08:56:46 -0700

    Moved execution-related primitives to a separate exec package

---
+++ pip-services4-components-node/src/exec/index.ts
@@ -0,0 +1,16 @@
+/** 
+ * @module exec 
+ * 
+ * Todo: Rewrite this description.
+ * 
+ * @preferred
+ */
+export { Parameters } from './Parameters';
+
+export { IParameterized } from './IParameterized';
+export { IExecutable } from './IExecutable';
+export { INotifiable } from './INotifiable';
+export { Executor } from './Executor';
+export { Notifier } from './Notifier';
+
+export { FixedRateTimer } from './FixedRateTimer';
--- pip-services4-components-node/test/exec/FixedRateTimer.test.ts
+++ pip-services4-components-node/test/exec/FixedRateTimer.test.ts
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 
 import { IContext } from "../../src/context/IContext";
-import { INotifiable } from '../../src/run/INotifiable';
+import { INotifiable } from '../../src/exec/INotifiable';
 import { FixedRateTimer } from '../../src/run/FixedRateTimer';
 
 suite('FixedRateTimer', ()=> {
